b5d26f4a70617c433d18353e6b2b2eeff32cd3cf
Zhipeng Bao <56377647+zpbao@users.noreply.github.com>
2021-02-19 14:54:36 -0500

    Add files via upload

---
+++ stargan-v2/sample.sh
@@ -0,0 +1,5 @@
+python main.py --mode sample --num_domains 3 --resume_iter 100000 --w_hpf 0 \
+--checkpoint_dir expr/checkpoints/afhq \
+--result_dir expr/results/afhq \
+--src_dir assets/representative/afhq/src \
+--ref_dir assets/representative/afhq/ref \
